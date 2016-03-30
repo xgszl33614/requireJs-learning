0ec1171ad52af5666092d08feb5cc0fbec21b509
xgszl <727471025@qq.com>
2016-03-30 18:56:12 +0800

    a

--- www/app/main.js
+++ www/app/main.js
@@ -1,12 +1,11 @@
 define(function (require) {
-    // Load any app-specific modules
-    // with a relative require call,
-    // like:
+    //'./'代表当前目录，因为此文件是由ap.js通过requirejs(['app_easy/main'])载入的，路径前会自动添加app_easy;
     var messages = require('./messages');
+    //直接引入，基于页面(这里的页面是指加载requirejs的html页面，并不是main.js,也不是ap.js,)查找路径
+    var messages2 = require('app/messages.js');
 
-    // Load library/vendor modules using
-    // full IDs, like:
     var print = require('print');
 
     print(messages.getHello());
+    print(messages2.getHello());
 });
